1d3daa770a4fd825df118e0b68fb446c1c7a2f68
M1urray <rknjonjo@gmail.com>
2023-03-08 12:17:21 +0300

    add wsgi

--- app.py
+++ app.py
@@ -153,5 +153,5 @@ def api_predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0',port=8080,debug=False)
 
